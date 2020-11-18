fce0140df9588252e95c8069c2426aeabae782cd
thdk <t.dekiere@gmail.com>
2020-11-18 14:07:09 +0100

    Analyze tree shaking

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useGesture } from 'react-use-gesture';
 
 function App() {
+   const gestures = useGesture({
+    onDragEnd: (data) => {
+      console.log({data});
+    },
+  });
   return (
-    <div className="App">
+    <div 
+    className="App"
+    {...gestures()}
+    >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Data is logged to console for every drag gesture (onDragEnd)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
